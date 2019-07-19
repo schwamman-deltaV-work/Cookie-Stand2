aec5a61a4076094abb5289a50e14312ed35192d8
Calvin G Hall <123chall@gmail.com>
2019-07-19 12:51:37 -0500

    Fixed maps

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,10 +1,14 @@
-function myMap() {
+function createMap(id) {
     var mapProp= {
       center:new google.maps.LatLng(47.608638, -122.340138),
       zoom:15,
     };
 
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var map = new google.maps.Map(document.getElementById(id), mapProp);
 }
 
-
+function createMaps() {
+  createMap('googleMap1');
+  createMap('googleMap2');
+  createMap('googleMap3');
+}
