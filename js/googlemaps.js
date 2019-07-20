3e5693609c4ea821426ef0b246e0ec242edc594f
Jonathon Schwamman <schwamman@msn.com>
2019-07-20 16:27:38 -0500

    Changed lat,long for google location maps

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,14 +1,17 @@
-function createMap(id) {
+function createMap(id, latX, latY) {
     var mapProp= {
-      center:new google.maps.LatLng(47.608638, -122.340138),
+      center:new google.maps.LatLng(latX, latY),
       zoom:15,
     };
 
-    var map = new google.maps.Map(document.getElementById(id), mapProp);
+  new google.maps.Map(document.getElementById(id), mapProp);
 }
 
+
 function createMaps() {
-  createMap('googleMap1');
-  createMap('googleMap2');
-  createMap('googleMap3');
+  createMap('googleMap1', 47.608638, -122.340138);
+  createMap('googleMap2', 47.443638, -122.300444);
+  createMap('googleMap3', 47.620745, -122.350433);
+  createMap('googleMap4', 47.623776, -122.320630);
+  createMap('googleMap5', 47.578794, -122.411136);
 }
